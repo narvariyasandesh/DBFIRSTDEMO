23644b41bd989e0cf75b2f9b0abd065cbce68e75
sande <sande@DESKTOP-KIBBS2E>
2022-03-07 09:58:52 +0530

    Add project files.

---
+++ DBFIRSTDEMO/Controllers/CustomerController.cs
@@ -0,0 +1,80 @@
+﻿using DBFIRSTDEMO.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DBFIRSTDEMO.Controllers
+{
+    public class CustomerController : Controller
+    {
+        DBFIRSTCUSTOMEREntities context = new DBFIRSTCUSTOMEREntities();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(context.Customers.ToList());
+        }
+
+        public ActionResult Create(Customer cust)
+        {
+            ViewBag.member = context.Memberships.ToList();
+            if (cust.id==0)
+            {
+                return View();
+            }
+            return View(cust);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddCustomer(Customer customer)
+        {
+            ViewBag.member = context.Memberships.ToList();
+            if (customer != null)
+            {
+                Customer cust = new Customer();
+                cust.id = customer.id;
+                cust.firstname = customer.firstname;
+                cust.lastname = customer.lastname;
+                cust.email = customer.email;
+                cust.mobile = customer.mobile;
+                cust.MembershipId = customer.MembershipId;
+                //cust.Membership = customer.Membership;
+                if (cust.id == 0)
+                {
+                    context.Customers.Add(cust);
+                    context.SaveChanges();
+                    //try
+                    //{
+                    //    context.SaveChanges();
+                    //}
+                    //catch(DbEntityValidationException e)
+                    //{
+                    //    Console.WriteLine(e);
+                    //}
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    context.Entry(cust).State = System.Data.Entity.EntityState.Modified;
+                    context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var cust = context.Customers.Where(x => x.id == id).First();
+            if (cust != null)
+            {
+                context.Customers.Remove(cust);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DBFIRSTDEMO/Controllers/MembershipController.cs
@@ -0,0 +1,45 @@
+﻿using DBFIRSTDEMO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DBFIRSTDEMO.Controllers
+{
+    public class MembershipController : Controller
+    {
+        DBFIRSTCUSTOMEREntities context = new DBFIRSTCUSTOMEREntities();
+        // GET: Membership
+        public ActionResult Index()
+        {
+            ViewBag.member = context.Memberships.ToList();
+            return View();
+        }
+
+
+        public ActionResult AddMembership(Membership membership)
+        {
+            
+            Membership member = new Membership();
+            member.membertype = membership.membertype;
+            context.Memberships.Add(member);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+          
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            var member = context.Memberships.Where(e => e.id == id).First();
+            if (member != null)
+            {
+                context.Memberships.Remove(member);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
